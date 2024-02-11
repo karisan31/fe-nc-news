acc616b299bc0feb4cb837e65c1703f76890d49a
karisan31 <kmathan31@outlook.com>
2024-02-11 01:45:25 +0000

    added error handling for bad requests

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,4 +1,17 @@
 export default function Error({ error }) {
+  if (!error) {
+    return (
+      <>
+        <div className="jumbotron jumbotron-fluid py-7 mt-5">
+          <div className="container">
+            <h1 className="display-4">Uh Oh! Error 400</h1>
+            <hr className="my-4" />
+            <p className="lead">That was a Bad Request!</p>
+          </div>
+        </div>
+      </>
+    );
+  }
   if (error.msg) {
     return <p>{error.msg}</p>;
   }
@@ -8,6 +21,7 @@ export default function Error({ error }) {
         <div className="container">
           <h1 className="display-4">Uh Oh! Error {error.status}</h1>
           <hr className="my-4" />
+          {error.status === 400 && <p className="lead">{error.data.msg}!</p>}
           {error.status === 404 && (
             <p className="lead">It seems as though this {error.data.msg}!</p>
           )}
